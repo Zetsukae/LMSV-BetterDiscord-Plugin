1543295e4bef83475ca12c619dde347208e74535
Zetsukae <dangeloalexis2@gmail.com>
2025-04-23 10:06:12 +0200

    Update MultiLeavePlugin.plugin.js

--- MultiLeavePlugin.plugin.js
+++ MultiLeavePlugin.plugin.js
@@ -2,7 +2,7 @@
  * @name LeaveMultipleServersVanilla
  * @author Zetsukae
  * @version 1.2.2
- * @description Leave multiple servers at once, with server icons, names — V.1.2.1 / only created by Zetsukae
+ * @description Leave multiple servers at once, with server icons, names — V.1.2.2 / only created by Zetsukae
  * @github https://github.com/Zetsukae/MultiLeaveBDPlugin
  */
 
